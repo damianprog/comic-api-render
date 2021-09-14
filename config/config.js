91c4ef51af5aa39de4a5d6393e7c72f216eb691e
damianprog <damianpukszta@gmail.com>
2021-09-14 10:21:21 +0200

    removed old code from config.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,6 @@
 require('dotenv').config();
 
-// const devConfig = {
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-//   port: process.env.DB_PORT,
-//   host: process.env.DB_HOST,
-//   dialect: 'postgres',
-// };
-
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-// const proConfig = {
-//   connectionString: process.env.DATABASE_URL,
-// };
 
 const proConfig = process.env.DATABASE_URL;
 
