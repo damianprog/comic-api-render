fb73fbc0839fabadc310cbfa82170c13d1c49660
Damian Pukszta <damian@192.168.1.2>
2024-10-08 15:03:04 +0200

    Changed models index.js config path

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.js')[env];
+// const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.js')['development'];
 const db = {};
 
 // let sslConfig = { dialect: 'postgres' };
